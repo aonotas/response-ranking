e6e30f8b1ccdfdd8f92ef3256fb0cf14f7787cae
aonotas <nanigashi03@gmail.com>
2017-07-14 14:17:31 +0900

    add chainer ver

--- code/adr_res_selection/main/chainer_net.py
+++ code/adr_res_selection/main/chainer_net.py
@@ -76,7 +76,7 @@ class ConversationEncoderGRU(chainer.Chain):
 
         # Extract Last Vector
         cumsum_idx = xp.cumsum(lengths)
-        last_idx = cumsum_idx - 1
+        last_idx = cumsum_idx - xp.ones(cumsum_idx.shape, dtype=xp.int32)
         agent_vecs = F.embed_id(last_idx, F.concat(ys, axis=0))
         agent_vecs = F.dropout(agent_vecs, ratio=self.use_dropout)
 
