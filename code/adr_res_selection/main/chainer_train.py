34e0b89b463ecc10ddb47b556646cebd61d9be07
aonotas <nanigashi03@gmail.com>
2017-07-17 23:53:09 +0900

    debug

--- code/adr_res_selection/main/chainer_train.py
+++ code/adr_res_selection/main/chainer_train.py
@@ -27,6 +27,8 @@ import chainer_util as ch_util
 
 to_gpu = chainer.cuda.to_gpu
 
+from ..utils.evaluator import Evaluator
+
 
 def get_datasets(argv):
     say('\nSET UP DATASET\n')
@@ -104,70 +106,6 @@ def create_samples(argv, train_dataset, dev_dataset, test_dataset, vocab_word):
     if test_samples:
         say('\nTest samples\tMini-Batch:%d' % len(test_samples))
     return train_samples, dev_samples, test_samples, n_train_batches, evalset
-
-
-# def train(argv, model_api, n_train_batches, evalset, dev_samples, test_samples):
-#     say('\n\nTRAINING START\n')
-#
-#     acc_history = {}
-#     best_dev_acc_both = 0.
-#     unchanged = 0
-#
-#     batch_indices = range(n_train_batches)
-#
-#     for epoch in xrange(argv.epoch):
-#         ##############
-#         # Early stop #
-#         ##############
-#         unchanged += 1
-#         if unchanged > 5:
-#             say('\n\nEARLY STOP\n')
-#             break
-#
-#         ############
-#         # Training #
-#         ############
-#         say('\n\n\nEpoch: %d' % (epoch + 1))
-#         say('\n  TRAIN  ')
-#         model_api.train_all(batch_indices, evalset)
-#
-#         ##############
-#         # Validating #
-#         ##############
-#         if dev_samples:
-#             say('\n\n  DEV  ')
-#             dev_acc_both, dev_acc_adr, dev_acc_res = model_api.predict_all(dev_samples)
-#
-#             if dev_acc_both > best_dev_acc_both:
-#                 unchanged = 0
-#                 best_dev_acc_both = dev_acc_both
-#                 acc_history[epoch + 1] = [(best_dev_acc_both, dev_acc_adr, dev_acc_res)]
-#
-#                 if argv.save:
-#                     #                    model_api.save_model(argv.output_fn)
-#                     model_api.save_params(argv.output_fn + '_epoch' + str(epoch))
-#
-#         if test_samples:
-#             say('\n\n\r  TEST  ')
-#             test_acc_both, test_acc_adr, test_acc_res = model_api.predict_all(test_samples)
-#
-#             if unchanged == 0:
-#                 if epoch + 1 in acc_history:
-#                     acc_history[epoch + 1].append((test_acc_both, test_acc_adr, test_acc_res))
-#                 else:
-#                     acc_history[epoch + 1] = [(test_acc_both, test_acc_adr, test_acc_res)]
-#
-#         #####################
-#         # Show best results #
-#         #####################
-#         say('\n\tBEST ACCURACY HISTORY')
-#         for k, v in sorted(acc_history.items()):
-#             text = '\n\tEPOCH-{:>3} | DEV  Both:{:>7.2%}  Adr:{:>7.2%}  Res:{:>7.2%}'
-#             text = text.format(k, v[0][0], v[0][1], v[0][2])
-#             if len(v) == 2:
-#                 text += ' | TEST  Both:{:>7.2%}  Adr:{:>7.2%}  Res:{:>7.2%}'
-#                 text = text.format(v[1][0], v[1][1], v[1][2])
-#             say(text)
 
 
 def main():
